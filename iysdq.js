2bb5ad6c298c4bf34216c2499f8daa156a86eef7
anmu2013 <108502178+anmu2013@users.noreply.github.com>
2026-08-17 14:26:41 +0800

    iysdq.js

--- iysdq.js
+++ iysdq.js
@@ -1,25 +1,14 @@
-var rule = {
-    title: '影视大全',
-    host: 'https://iysdq.tv',
-    url: '/vodshow/fyclass/page/fypage.html',
-    searchUrl: '/vodsearch/page/fypage/wd/fyfilter.html',
-    searchable: 2,
-    quickSearch: 1,
-    filterable: 0,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; TVBox) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-        'Referer': 'https://iysdq.tv/'
-    },
-    class_name: '电影&电视剧&综艺&动漫',
-    class_url: '1&2&3&4',
-    推荐: '.vod-list-item;a&&title;img&&data-original;.remarks&&Text;a&&href',
-    一级: '.vod-list-item;a&&title;img&&data-original;.remarks&&Text;a&&href',
-    二级: {
-        "title": "h1&&Text;.vod-detail-info&&span:eq(1)&&Text",
-        "img": ".vod-detail-pic&&img&&src",
-        "desc": ".vod-detail-content&&Text",
-        "tabs": ".playlist-tab&&li",
-        "lists": ".playlist&&ul:eq(#id)&&li"
-    },
-    搜索: '.search-result-item;a&&title;img&&src;.remarks&&Text;a&&href',
+{
+  "主页url": "https://iysdq.tv",
+  "请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36#Referer$https://iysdq.tv/",
+  "起始页": "1",
+  "分类url": "https://iysdq.tv/vodshow/{fyclass}/page/{fypage}.html",
+  "分类": "电影$1#电视剧$2#综艺$3#动漫$4",
+  "分类明细": ".vod-list-item;a&&title;img&&data-original;.remarks&&Text;a&&href",
+  "详情": "h1&&Text;.vod-detail-pic&&img&&src;.vod-detail-content&&Text",
+  "线路": ".playlist-tab&&li",
+  "播放列表": ".playlist&&ul:eq(#id)&&li",
+  "播放集数": "a&&Text;a&&href",
+  "搜索url": "https://iysdq.tv/vodsearch/page/{fypage}/wd/{key}.html",
+  "搜索明细": ".search-result-item;a&&title;img&&src;.remarks&&Text;a&&href"
 }
